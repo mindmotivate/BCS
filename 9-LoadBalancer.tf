8ff99abf7c5c705bf0f4eb57a151ed21bac72bf4
malgus-waf <129628996+malgus-waf@users.noreply.github.com>
2023-11-14 21:37:47 -0600

    Add files via upload
    This is for 443

---
+++ 9-LoadBalancer.tf
@@ -0,0 +1,53 @@
+resource "aws_lb" "app1_alb" {
+  name               = "app1-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.app1-443-sg02-LB01.id]
+  subnets            = [
+    aws_subnet.public-eu-west-1a.id,
+    aws_subnet.public-eu-west-1b.id,
+    aws_subnet.public-eu-west-1c.id
+  ]
+  enable_deletion_protection = false
+
+  tags = {
+    Name    = "App1LoadBalancer"
+    Service = "App1"
+    Owner   = "Chewbacca"
+    Project = "Web Service"
+  }
+}
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app1_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app1_tg.arn
+  }
+}
+
+data "aws_acm_certificate" "cert" {
+  domain   = "balericacloudsec.com"
+  statuses = ["ISSUED"]
+  most_recent = true
+}
+
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.app1_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"  # or whichever policy suits your requirements
+  certificate_arn   = data.aws_acm_certificate.cert.arn
+
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app1_tg.arn
+  }
+}
