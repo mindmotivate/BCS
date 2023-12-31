bd80cd412d94046fa974858658f354935acbd11f
mindmotivate <130941970+mindmotivate@users.noreply.github.com>
2023-12-30 22:21:28 -0500

    Update 2-Subnets.tf

--- 2-Subnets.tf
+++ 2-Subnets.tf
@@ -1,6 +1,6 @@
 #These are   for  public
 
-resource "aws_subnet" "public-eu-west-1a" {
+resource "aws_subnet" "public-us-east-1a" {
   vpc_id                  = aws_vpc.app1.id
   cidr_block              = "10.32.1.0/24"
   availability_zone       = "us-east-1a"
@@ -14,7 +14,7 @@ resource "aws_subnet" "public-eu-west-1a" {
   }
 }
 
-resource "aws_subnet" "public-eu-west-1b" {
+resource "aws_subnet" "public-us-east-1b" {
   vpc_id                  = aws_vpc.app1.id
   cidr_block              = "10.32.2.0/24"
   availability_zone       = "us-east-1b"
